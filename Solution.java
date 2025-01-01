5c1d9cd2247a31f21a802e90d70c7a83893cfc40
dj258255 <dj258255@naver.com>
2025-01-01 22:16:15 +0900

    SWEA_D1(2063)_중간값 찾기
    EASY

--- Solution.java
+++ Solution.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String number = scan.next();
-        int answer =0;
-        for(int i = 0 ; i < number.length() ; i++){
-            answer += Integer.parseInt(number.substring(i,i+1));
+        int n = scan.nextInt(); // N
+        int[] numbers = new int[n];
+
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = scan.nextInt();
         }
 
-        System.out.println(answer);
+        Arrays.sort(numbers);
+        System.out.println(numbers[numbers.length/2]);
+
     }
 }
