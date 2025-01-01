2fe96d70923b2a73318606039665f8d26e8dfd8c
dj258255 <dj258255@naver.com>
2025-01-01 21:54:10 +0900

    SWEA_D1_(Rock paper scissors)
    EASY

--- Solution.java
+++ Solution.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Solution {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        if (A < B) {
+            if(A == 1 && B ==3) {
+                System.out.println("A");
+            } else
+                System.out.println("B");
+        }
+        else
+            System.out.println("A");
     }
 }
