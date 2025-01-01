397a2a83f7936701f58e3367aad90cb255800ce3
dj258255 <dj258255@naver.com>
2025-01-01 22:02:31 +0900

    SWEA_D1(2058 자릿수 더하기)
    easy

--- Solution.java
+++ Solution.java
@@ -4,16 +4,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int A = scan.nextInt();
-        int B = scan.nextInt();
-
-        if (A < B) {
-            if(A == 1 && B ==3) {
-                System.out.println("A");
-            } else
-                System.out.println("B");
+        String number = scan.next();
+        int answer =0;
+        for(int i = 0 ; i < number.length() ; i++){
+            answer += Integer.parseInt(number.substring(i,i+1));
         }
-        else
-            System.out.println("A");
+
+        System.out.println(answer);
     }
 }
