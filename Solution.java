84734808945d50fa0509dad5af569027dd5adbfe
dj258255 <dj258255@naver.com>
2025-01-02 18:33:14 +0900

    SWEA_D1(1959)_두 개의 숫자열
    역시 이지하군요 ㅋ

--- Solution.java
+++ Solution.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        for(int t = 1; t <= T; t++) {
 
-        int n = scan.nextInt(); // N
-        int[] numbers = new int[n];
+            int N = scan.nextInt();
+            int M = scan.nextInt();
 
-        for(int i = 0; i < numbers.length; i++){
-            numbers[i] = scan.nextInt();
+            int[] A = new int[N];
+            int[] B = new int[M];
+
+            for(int i = 0; i < N; i++) {
+                A[i] = scan.nextInt();
+            }
+            for(int i = 0; i < M; i++) {
+                B[i] = scan.nextInt();
+            }
+            int[] longer = N >=M ? A : B;
+            int[] shorter = N < M ? A : B;
+
+            int max = Integer.MIN_VALUE;
+
+            for(int i = 0; i <= longer.length - shorter.length; i++){
+                int answer =0;
+                for(int j = 0; j < shorter.length; j++) {
+                    answer += shorter[j] * longer[i+j];
+                }
+                max = Integer.max(answer, max);
+            }
+
+            System.out.println("#" + t + " " + max);
+
         }
-
-        Arrays.sort(numbers);
-        System.out.println(numbers[numbers.length/2]);
 
     }
 }
