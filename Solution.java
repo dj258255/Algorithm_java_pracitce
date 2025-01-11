4823ed8aff94c375cd57964f4d06b1d79ea7aecf
dj258255 <dj258255@naver.com>
2025-01-11 17:26:30 +0900

    SWEA_D2(2001)_파리 퇴치
    은근 쉬웠음

--- Solution.java
+++ Solution.java
@@ -4,34 +4,37 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int T = scan.nextInt();
-
         for (int t = 1; t <= T; t++) {
             int N = scan.nextInt();
-            System.out.println("#" + t);
+            int M = scan.nextInt();
+            int[][] map = new int[N][N];
 
-            int[][] pascal = new int[N][N];
-
-            // 파스칼의 삼각형 생성
             for (int i = 0; i < N; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (j == 0 || j == i) {
-                        pascal[i][j] = 1; // 각 행의 첫 번째와 마지막 숫자는 1
-                    } else {
-                        pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
-                    }
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = scan.nextInt();
                 }
             }
 
-            // 파스칼의 삼각형 출력
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j <= i; j++) {
-                    System.out.print(pascal[i][j]);
-                    if (j < i) {
-                        System.out.print(" "); // 숫자들 사이에 빈 칸 추가
+            int maxFlies = Integer.MIN_VALUE;
+
+            // M x M 크기의 파리채로 파리를 잡는 최대값 계산
+            for (int i = 0; i <= N - M; i++) {
+                for (int j = 0; j <= N - M; j++) {
+                    int currentFlies = 0;
+
+                    // M x M 영역 내 파리 수 합산
+                    for (int k = 0; k < M; k++) {
+                        for (int l = 0; l < M; l++) {
+                            currentFlies += map[i + k][j + l];
+                        }
                     }
+
+                    maxFlies = Math.max(maxFlies, currentFlies);
                 }
-                System.out.println(); // 다음 줄로 이동
             }
+
+            System.out.println("#" + t + " " + maxFlies);
         }
+        scan.close();
     }
 }
