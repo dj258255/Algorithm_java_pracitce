6e51f6557c19afee7a38a9089b1797d8e2a4acd7
dj258255 <dj258255@naver.com>
2025-01-11 18:29:09 +0900

    swea_D2(회문검사)_1989
    회문검사이다

--- Solution.java
+++ Solution.java
@@ -4,37 +4,16 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int T = scan.nextInt();
+
         for (int t = 1; t <= T; t++) {
-            int N = scan.nextInt();
-            int M = scan.nextInt();
-            int[][] map = new int[N][N];
+            String word = scan.next();
+            StringBuilder reverseWord = new StringBuilder(word).reverse();
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = scan.nextInt();
-                }
+            if (word.equals(reverseWord.toString())) {
+                System.out.println("#" + t + " 1");
+            } else {
+                System.out.println("#" + t + " 0");
             }
-
-            int maxFlies = Integer.MIN_VALUE;
-
-            // M x M 크기의 파리채로 파리를 잡는 최대값 계산
-            for (int i = 0; i <= N - M; i++) {
-                for (int j = 0; j <= N - M; j++) {
-                    int currentFlies = 0;
-
-                    // M x M 영역 내 파리 수 합산
-                    for (int k = 0; k < M; k++) {
-                        for (int l = 0; l < M; l++) {
-                            currentFlies += map[i + k][j + l];
-                        }
-                    }
-
-                    maxFlies = Math.max(maxFlies, currentFlies);
-                }
-            }
-
-            System.out.println("#" + t + " " + maxFlies);
         }
-        scan.close();
     }
 }
