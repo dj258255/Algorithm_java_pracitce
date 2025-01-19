3b617f218d91a864c57ef9b3e5309a310813a893
dj258255 <dj258255@naver.com>
2025-01-19 21:05:52 +0900

    SWEA_D3_입국심사
    ㅇㅇ

--- Solution.java
+++ Solution.java
@@ -1,53 +1,53 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        for (int T = 1; T <= 10; T++) {
-            int t = scan.nextInt();
-            char[][] map = new char[100][100];
+        int t = scan.nextInt();
 
-            // 2차원 배열 입력
-            for (int i = 0; i < 100; i++) {
-                String temp = scan.next();
-                for (int j = 0; j < 100; j++) {
-                    map[i][j] = temp.charAt(j);
-                }
+        for (int T = 1; T <= t; T++) {
+            int n = scan.nextInt();
+            int m = scan.nextInt();
+
+            int[] times = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                times[i] = scan.nextInt();
             }
 
-            int max = 0;
+            Arrays.sort(times);
 
-            // 행 방향 회문 확인
-            for (int i = 0; i < 100; i++) {
-                for (int j = 0; j < 100; j++) {
-                    for (int k = 1; j + k <= 100; k++) {
-                        StringBuilder str = new StringBuilder();
-                        for (int l = 0; l < k; l++) {
-                            str.append(map[i][j + l]);
-                        }
-                        if (str.toString().equals(str.reverse().toString())) {
-                            max = Math.max(max, k);
-                        }
-                    }
-                }
-            }
-
-            // 열 방향 회문 확인
-            for (int j = 0; j < 100; j++) {
-                for (int i = 0; i < 100; i++) {
-                    for (int k = 1; i + k <= 100; k++) {
-                        StringBuilder str = new StringBuilder();
-                        for (int l = 0; l < k; l++) {
-                            str.append(map[i + l][j]);
-                        }
-                        if (str.toString().equals(str.reverse().toString())) {
-                            max = Math.max(max, k);
-                        }
-                    }
-                }
-            }
-
-            System.out.println("#" + t + " " + max);
+            System.out.println("#" + T + " " + BinarySearch(times, m));
         }
+    }
+
+    public static long BinarySearch(int[] times, int m) {
+        long left = 1, right = (long) times[times.length - 1] * m;
+        long mid = 0; // 이진 탐색에서 반드시 left, right, mid값을 사용해야 함
+        long ans = right;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+
+            // 이 시간이 가능하다면, 최소 시간을 갱신하고 더 작은 시간을 찾기 위해 right를 조정
+            if (isPassed(times, mid) >= m) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    static long isPassed(int[] times,long mid) {
+        long amount = 0; // 심사 받은 사람의 수
+
+        for (int i = 0; i < times.length; i++) {
+            amount += mid / times[i];
+        }
+
+        return amount;
     }
 }
