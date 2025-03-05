e9f5b222a0370b281ae0ad812f6d04033be508a5
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-03-05 10:30:56 +0900

    [level 3] Title: 입국심사, Time: 99.78 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,17 +1,21 @@
 import java.util.Arrays;
-
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
+        
         Arrays.sort(times);
+        
         long left = 0;
-        long right = times[times.length-1] * (long) n;
+        long right = times[times.length-1] * (long)n;
+        
         while(left <= right){
-            long mid = (left + right) /2;
+            long mid = (left+right)/2;
             long complete = 0;
             
-            for(int i = 0 ; i < times.length; i++)
+            for(int i = 0 ; i < times.length; i++){
                 complete += mid / times[i];
+            }
+            
             
             if(complete < n){
                 left = mid + 1;
@@ -21,7 +25,6 @@ class Solution {
             }
         }
         
-        
-     return answer;   
+        return answer;
     }
 }
