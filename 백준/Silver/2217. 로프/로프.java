7cfb80586605012b091d38a8e92da6f83fefd557
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-03-09 01:43:10 +0900

    [Silver IV] Title: 로프, Time: 332 ms, Memory: 31088 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,39 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-
-        int[] num = new int[N];
-        int big = 0;
-        for(int i = 0; i < N; i++){
+        int n = Integer.parseInt(st.nextToken());
+        int[] w = new int[n];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            num[i] = Integer.parseInt(st.nextToken());
-            big = Math.max(big, num[i]); //제일 큰 값 찾기
+            w[i] = Integer.parseInt(st.nextToken());
         }
-
-        int answer = 0;
-
-        for(int i = 1 ; i <= big; i++){
-            int sum = 0;
-
-            for(int j = 0; j < N; j++){
-                if(num[j] >= i) {
-                    sum += i;
-                }
-            }
-
-            answer = Math.max(answer, sum);
+        Arrays.sort(w);
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans = Math.max(ans, w[n - i] * i);
         }
-
-        System.out.println(answer);
-
-
+        System.out.println(ans);
     }
 }
