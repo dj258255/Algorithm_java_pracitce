37ff093e6341e0c18af1a1860487449eadce1bd8
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-03-11 22:04:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 18340 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long K = Long.parseLong(st.nextToken());
+        long N = Long.parseLong(st.nextToken());
+        long right = 0;
+        long[] number = new long[(int)K];
+
+        for (int i = 0; i < (int)K; i++) {
+            st = new StringTokenizer(br.readLine());
+            number[i] = Long.parseLong(st.nextToken());
+            right = Math.max(right, number[i]);
+        }
+
+        long answer = 0;
+        long left = 1;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+            for (int i = 0; i < (int)K; i++) {
+                count += number[i] / mid;
+            }
+
+            if (count >= N) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
