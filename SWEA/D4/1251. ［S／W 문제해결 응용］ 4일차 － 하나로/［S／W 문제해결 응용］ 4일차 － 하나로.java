c8e6ba9ea08a2abc4919dd657edeb5f528cb5edb
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-03-31 17:02:08 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 216 ms, Memory: 35,888 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,65 +1,17 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-    
-    static int[] parents;
-    static int N; 
-    
-    static void make() {
-        for (int i = 0; i < N; i++) {
-            parents[i] = i;
-        }
-    }
-    
-    static int find(int x) {
-        if (x == parents[x]) return x;
-        return parents[x] = find(parents[x]);
-    }
-    
-    static boolean union(int a, int b) {
-        int aRoot = find(a);
-        int bRoot = find(b);
-        if (aRoot == bRoot) return false;
-        parents[bRoot] = aRoot;
-        return true;
-    }
-    
-    static class Edge implements Comparable<Edge> {
-        int from, to;
-        long weight;
-        
-        public Edge(int from, int to, long weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
-        }
-        
-        @Override
-        public int compareTo(Edge o) {
-            return Long.compare(this.weight, o.weight);
-        }
-    }
-    
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
         
-        int T = Integer.parseInt(br.readLine().trim());
-        StringBuilder sb = new StringBuilder();
-        
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine().trim());
-            parents = new int[N];
-            make();
-            
+        for (int t = 1; t <= T; t++) {
+            int N = Integer.parseInt(br.readLine());
             long[] x = new long[N];
             long[] y = new long[N];
             
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 x[i] = Long.parseLong(st.nextToken());
             }
@@ -69,37 +21,42 @@ public class Solution {
                 y[i] = Long.parseLong(st.nextToken());
             }
             
-            double E = Double.parseDouble(br.readLine().trim());
+            double E = Double.parseDouble(br.readLine());
             
-            int edgeCount = N * (N - 1) / 2;
-            Edge[] edges = new Edge[edgeCount];
-            int idx = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    long dx = x[i] - x[j];
-                    long dy = y[i] - y[j];
-                    long distSquared = dx * dx + dy * dy;
-                    edges[idx++] = new Edge(i, j, distSquared);
-                }
-            }
-            
-            Arrays.sort(edges);
-            
-            long mstCost = 0;
-            int edgeUsed = 0;
-            for (Edge edge : edges) {
-                if (union(edge.from, edge.to)) {
-                    mstCost += edge.weight;
-                    edgeUsed++;
-                    if (edgeUsed == N - 1) break;
-                }
-            }
-            
+            boolean[] visited = new boolean[N];
+            double[] minDist = new double[N];
+            Arrays.fill(minDist, Double.MAX_VALUE);
 
-            long result = Math.round(mstCost * E);
-            sb.append("#").append(tc).append(" ").append(result).append("\n");
+            minDist[0] = 0;
+            double result = 0;
+            
+            for (int i = 0; i < N; i++) {
+                int u = -1;
+                double min = Double.MAX_VALUE;
+                for (int j = 0; j < N; j++) {
+                    if (!visited[j] && minDist[j] < min) {
+                        min = minDist[j];
+                        u = j;
+                    }
+                }
+                
+                visited[u] = true;
+                result += min;
+                
+                for (int v = 0; v < N; v++) {
+                    if (!visited[v]) {
+                        long dx = x[u] - x[v];
+                        long dy = y[u] - y[v];
+                        double cost = dx * dx + dy * dy;
+                        if (cost < minDist[v]) {
+                            minDist[v] = cost;
+                        }
+                    }
+                }
+            }
+            
+            long answer = Math.round(result * E);
+            System.out.println("#" + t + " " + answer);
         }
-        
-        System.out.print(sb.toString());
     }
 }
