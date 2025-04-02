e27e114026842890e094c35bce7899d631277bd1
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-02 13:08:57 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 112 ms, Memory: 32,128 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -2,162 +2,78 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Solution{
-	static int N,M,R,C,L;
-	static ArrayList<int[]> list;
-	static int[][] map;
-	static int[][] move = {{-1,0},{1,0},{0,-1},{0,1}}; //상 하 좌 우
-	static int count;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
-		int Tc = Integer.parseInt(st.nextToken());
-		for(int T = 1; T<=Tc; T++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			M = Integer.parseInt(st.nextToken());
-			R = Integer.parseInt(st.nextToken());
-			C = Integer.parseInt(st.nextToken());
-			L = Integer.parseInt(st.nextToken());
-			
-			map = new int[N][M];
-			for(int i = 0 ; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j = 0 ; j < M; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			bfs();
-			System.out.println("#" + T + " " + count);
-		}
-		
-	}
-	
-	public static void bfs() {
-		boolean[][] visited = new boolean[N][M];
-		Queue<int[]> queue = new ArrayDeque<>();
-		//시작 위치를 방문 처리하면서 count에 포함 (시작 셀 포함)
-		queue.add(new int[] {R, C, map[R][C], 0});
-		visited[R][C] = true;
-		count = 1;
+class Solution {
+    static int N, M, R, C, L;
+    static int[][] map;
+    static int[] pipe = new int[8];
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int[] opposite = {1, 0, 3, 2};
+    static int count;
 
-		while(!queue.isEmpty()){
-		    int[] node = queue.poll();
-		    int r = node[0];
-		    int c = node[1];
-		    int type = node[2];
-		    int level = node[3];
-		    
-		    if(level == L - 1) continue;
-		    
-		    for(int dir = 0; dir < 4; dir++) {
-		        if(possibletype(type, dir)) continue; //현재 파이프가 해당 방향으로 이동 가능한지 체크
-		        
-		        // 올바른 방향 인덱스 사용
-		        int nr = r + move[dir][0];
-		        int nc = c + move[dir][1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        pipe[1] = (1 << 0) | (1 << 1) | (1 << 2) | (1 << 3); // 타입 1: 상,하,좌,우
+        pipe[2] = (1 << 0) | (1 << 1);                         // 타입 2: 상,하
+        pipe[3] = (1 << 2) | (1 << 3);                         // 타입 3: 좌,우
+        pipe[4] = (1 << 0) | (1 << 3);                         // 타입 4: 상,우
+        pipe[5] = (1 << 1) | (1 << 3);                         // 타입 5: 하,우
+        pipe[6] = (1 << 1) | (1 << 2);                         // 타입 6: 하,좌
+        pipe[7] = (1 << 0) | (1 << 2);                         // 타입 7: 상,좌
 
+        int Tc = Integer.parseInt(br.readLine().trim());
+        for (int T = 1; T <= Tc; T++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+            C = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
 
-		        if(board(nr, nc)) continue; //맵 경계 체크 또는 파이프가 없으면 건너뜀
-		        
-		        int ntype = map[nr][nc];
-		        if(possibleFind(ntype, dir)) continue; //파이프가 연결 가능한지 체크
-		        
-		        if(!visited[nr][nc]) {
-		            visited[nr][nc] = true;
-		            queue.add(new int[] {nr, nc, ntype, level + 1});
-		            count++;
-		        }
-		    }
-		}
+            map = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 
-		
-		
-	}
-	
-	//이동할 수 있는지 없는지
-	public static boolean possibletype(int type, int d) {
-		if(d==0) { //위쪽으로 이동가능?
-			switch(type) {
-			case 3:
-			case 5:
-			case 6:
-				return true; //이넘들은 위로 못감
-			}
-		} else if(d==1) { //아래쪽으로 이동 가능?
-			switch(type){
-			case 3:
-			case 4:
-			case 7:
-				return true; // 이넘들은 아래로 못감
-			}
-		} else if(d==2) {
-			switch(type) {
-			case 2:
-			case 4:
-			case 5:
-				return true; //이넘들은 왼쪽으로 못감
-			}
-		} else if(d==3) {
-			switch(type) {
-			case 2:
-			case 6:
-			case 7:
-				return true; //이넘들은 오른쪽으로 못감
-			}
-		}
-	
-		return false;
-	}
-	
-	public static boolean board(int r,int c) {
-		if(r < 0 || r >= N || c < 0 || c >= M) return true;
-		if(map[r][c] == 0) return true; //파이프없으면
-		
-		return false;
-	}
+            bfs();
+            System.out.println("#" + T + " " + count);
+        }
+    }
 
-	
-	public static boolean possibleFind(int type , int d) { 
-		//방향 0: 상 , 1:하 , 2:좌, 3:우
-		if(d==0) {
-			switch(type) {
-		    case 3:
-		    case 4:
-		    case 7:
-		        return true;
-			}
-		} else if(d==1) {
-			switch(type) {
-			case 3:
-			case 5:
-			case 6:
-				return true;
-			}
-		} else if(d==2) {
-			switch(type){
-			case 2:
-			case 6:
-			case 7:
-				return true;
-			}
-		} else if(d==3) {
-			switch(type) {
-			case 2:
-			case 4:
-			case 5:
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
+    public static void bfs() {
+        boolean[][] visited = new boolean[N][M];
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] { R, C });
+        visited[R][C] = true;
+        count = 1;
+
+        int steps = 1;
+        while (!queue.isEmpty() && steps < L) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                int r = cur[0], c = cur[1];
+                int curType = map[r][c];
+                int possible = pipe[curType];
+                for (int d = 0; d < 4; d++) {
+                    if ((possible & (1 << d)) == 0) continue;
+                    int nr = r + dr[d], nc = c + dc[d];
+                    if (nr < 0 || nr >= N || nc < 0 || nc >= M || map[nr][nc] == 0 || visited[nr][nc])
+                        continue;
+                    int nextType = map[nr][nc];
+                    if ((pipe[nextType] & (1 << opposite[d])) == 0) continue;
+                    visited[nr][nc] = true;
+                    queue.add(new int[] { nr, nc });
+                    count++;
+                }
+            }
+            steps++;
+        }
+    }
 }
