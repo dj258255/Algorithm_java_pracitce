95cb1f105a85d915924138ce4ccf51e338e29240
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-03 20:31:24 +0900

    [D3] Title: 햄버거 다이어트, Time: 111 ms, Memory: 29,116 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,37 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        
-        int T = Integer.parseInt(st.nextToken());
-        for(int Tc = 1; Tc<=T; Tc++) {
-        	st = new StringTokenizer(br.readLine());
-
-            int N = Integer.parseInt(st.nextToken()); //햄버거 개수
-            int L = Integer.parseInt(st.nextToken()); //내 최대 칼로리
-
-            int[] score = new int[N];  //햄버거들 점수 
-            int[] calorie = new int[N]; //햄버거 칼로리
-
-            for(int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                score[i] = Integer.parseInt(st.nextToken()); //햄버거 맛
-                calorie[i] = Integer.parseInt(st.nextToken()); //햄버거 칼로리
-            }
-            
-            int[] dp = new int[L + 1]; //최대칼로리 +1
-
-            for(int i = 0; i < N; i++) {
-                for(int j = L; j >= calorie[i]; j--) {
-                    dp[j] = Math.max(dp[j], dp[j - calorie[i]] + score[i]);
-                }
-            }
-            
-            System.out.println("#" + Tc + " " + dp[L]);
-        }
-    }
+class Solution{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int T = Integer.parseInt(st.nextToken());
+		for(int tc=1; tc<=T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int N = Integer.parseInt(st.nextToken());
+			int L = Integer.parseInt(st.nextToken());
+			
+			int[][] hamburger = new int[N][2];
+			for(int i = 0 ; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				hamburger[i][0] = Integer.parseInt(st.nextToken()); //맛
+				hamburger[i][1] = Integer.parseInt(st.nextToken()); //칼로리
+			}
+			
+			int[] dp = new int[L+1];
+			
+			for(int i = 0; i < N; i++) {
+				for(int j = L; j > hamburger[i][1]; j--) {
+					dp[j] = Math.max(dp[j], dp[ j- hamburger[i][1] ] + hamburger[i][0]);
+				}
+			}
+			
+			System.out.println("#" + tc + " " + dp[L]);
+			
+		}
+	}
 }
