e7ed476997261df02808317ce8b3eda626151716
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-09 17:34:13 +0900

    [Gold IV] Title: 탈출, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -5,118 +5,107 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int R,C;
-	static char[][] map;
-	static int answer;
-	static int goalR,goalC;
-	static int[][] count;
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		map = new char[R][C];
-		count = new int[R][C];	
-		
-		for(int i = 0 ; i < R; i++) {
-			st = new StringTokenizer(br.readLine());
-			String temp = st.nextToken();
-			for(int j = 0 ; j < C; j++) {
-				map[i][j] = temp.charAt(j);
-				if(map[i][j] == 'D') {
-					goalR = i;
-					goalC = j;
-				}
-			}
-		}
-		
-		answer = 0;
-		for(int i = 0 ; i < R; i++) {
-			for(int j = 0; j < C; j++) {
-				if(map[i][j] == 'S') {
-					bfs(i,j);
-				}
-			}
-		}
-		
-		System.out.println(answer == 0 ? "KAKTUS" : answer);
-	}
-	
-	
-	public static void bfs(int startR, int startC) {
-	    Queue<int[]> queue = new ArrayDeque<>();
-	    queue.add(new int[]{startR, startC});
-	    boolean[][] visited = new boolean[R][C];
-	    visited[startR][startC] = true;
+public class Main {
+    static int R, C;
+    static char[][] map;
+    static int[][] count;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int goalR, goalC;
 
-	    while (!queue.isEmpty()) {
-	        waterMap();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	        Queue<int[]> nextQueue = new ArrayDeque<>();
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-	        while (!queue.isEmpty()) {
-	            int[] node = queue.poll();
-	            int r = node[0];
-	            int c = node[1];
+        map = new char[R][C];
+        count = new int[R][C];
+        Queue<int[]> waterQueue = new ArrayDeque<>();
+        Queue<int[]> moveQueue = new ArrayDeque<>();
 
-	            for (int d = 0; d < 4; d++) {
-	                int nr = r + dr[d];
-	                int nc = c + dc[d];
+        for (int i = 0; i < R; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = row.charAt(j);
+                if (map[i][j] == 'D') {
+                    goalR = i;
+                    goalC = j;
+                } else if (map[i][j] == 'S') {
+                    moveQueue.add(new int[]{i, j});
+                } else if (map[i][j] == '*') {
+                    waterQueue.add(new int[]{i, j});
+                }
+            }
+        }
 
-	                if (nr < 0 || nc < 0 || nr >= R || nc >= C) continue;
-	                if (map[nr][nc] == '*' || map[nr][nc] == 'X') continue;
-	                if (!visited[nr][nc]) {
-	                    visited[nr][nc] = true;
-	                    count[nr][nc] = count[r][c] + 1;
-	                    if (nr == goalR && nc == goalC) {
-	                        answer = count[nr][nc];
-	                        return;
-	                    }
-	                    nextQueue.add(new int[]{nr, nc});
-	                }
-	            }
-	        }
+        int result = bfs(moveQueue, waterQueue);
+        System.out.println(result == -1 ? "KAKTUS" : result);
+    }
 
-	        queue = nextQueue;
-	    }
-	}
+    public static int bfs(Queue<int[]> moveQueue, Queue<int[]> waterQueue) {
+        boolean[][] visited = new boolean[R][C];
+        for (int[] start : moveQueue) {
+            visited[start[0]][start[1]] = true;
+        }
 
-	
-	
-	public static void waterMap() {
-		Queue<int[]> queue = new ArrayDeque<>();
-		
-		for(int i = 0 ; i < R; i++) {
-			for(int j = 0 ; j <C;j++) {
-				if(map[i][j] == '*') {
-					queue.add(new int[] {i,j});
-				}
-			}
-		}
+        while (!moveQueue.isEmpty()) {
+            // 물 먼저 퍼뜨리기
+            waterQueue = spreadWater(waterQueue);
 
-		while(!queue.isEmpty()) {
-			int[] node = queue.poll();
-			int r = node[0];
-			int c = node[1];
-			for(int d = 0; d< 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				if(nr < 0 || nc < 0 || nr >= R || nc >= C) continue; //맵밖으로 넘어가면
-				if(map[nr][nc] == '*' || map[nr][nc] == 'X' || map[nr][nc] == 'D') continue; //물이거나 벽이면
-				
-				map[nr][nc] = '*';
-			}
-		}
-		
-		
-		
-		
-	}
-	
-	
+            int size = moveQueue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = moveQueue.poll();
+                int r = cur[0];
+                int c = cur[1];
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    if (!inBounds(nr, nc) || visited[nr][nc]) continue;
+                    if (map[nr][nc] == '*' || map[nr][nc] == 'X') continue;
+
+                    count[nr][nc] = count[r][c] + 1;
+                    if (map[nr][nc] == 'D') {
+                        return count[nr][nc];
+                    }
+
+                    visited[nr][nc] = true;
+                    moveQueue.add(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static Queue<int[]> spreadWater(Queue<int[]> waterQueue) {
+        int size = waterQueue.size();
+        Queue<int[]> newWater = new ArrayDeque<>();
+
+        for (int i = 0; i < size; i++) {
+            int[] cur = waterQueue.poll();
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if (!inBounds(nr, nc)) continue;
+                if (map[nr][nc] == '.' || map[nr][nc] == 'S') {
+                    map[nr][nc] = '*';
+                    newWater.add(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return newWater;
+    }
+
+    public static boolean inBounds(int r, int c) {
+        return r >= 0 && c >= 0 && r < R && c < C;
+    }
 }
