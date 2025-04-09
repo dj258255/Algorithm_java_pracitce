db822543cad33be9c7ec20a4a737e41052dbfca5
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-09 16:18:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 102 ms, Memory: 27,776 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -4,71 +4,76 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int N,X;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int T = Integer.parseInt(st.nextToken());
         for (int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // 한변의 크기
-            int X = Integer.parseInt(st.nextToken()); // 경사로의 길이
-            int[][] map = new int[N][N];
+        	st = new StringTokenizer(br.readLine());
+        	N = Integer.parseInt(st.nextToken());
+        	X = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-
-            int answer = 0;
-
-            // 가로 검사
-            for (int i = 0; i < N; i++) {
-                if (check(map[i], X)) {
-                    answer++;
-                }
-            }
-
-            // 세로 검사
-            for (int i = 0; i < N; i++) {
-                int[] col = new int[N];
-                for (int j = 0; j < N; j++) {
-                    col[j] = map[j][i];
-                }
-                if (check(col, X)) {
-                    answer++;
-                }
-            }
-
-            System.out.println("#" + tc + " " + answer);
+        	
+        	int[][] map = new int[N][N];
+        	
+        	for(int i = 0; i < N; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for(int j = 0 ; j < N; j++) {
+        			map[i][j] = Integer.parseInt(st.nextToken());
+        		}
+        	}
+        	
+        	int answer = 0;
+        	
+        	for(int i = 0 ; i < N; i++) {
+        		if(check(map[i],X)) {
+        			answer++;
+        		}
+        	}
+        	
+        	for(int i = 0 ; i < N; i++) {
+        		int[] col = new int[N];
+        		for(int j = 0 ; j < N; j++) {
+        			col[j] = map[j][i];
+        		}
+        		if(check(col,X)) {
+        			answer++;
+        		}
+        	}
+        	
+        	
+        	
+        	
+        	System.out.println("#" + tc + " " + answer );
         }
     }
-
-    private static boolean check(int[] line, int X) {
-        int N = line.length;
-        boolean[] installed = new boolean[N]; // 경사로 설치 여부 체크
-
-        for (int i = 0; i < N - 1; i++) {
-            if (line[i] == line[i + 1]) continue; // 높이가 같으면 패스
-
-            // 높이 차이가 1보다 크면 불가능
-            if (Math.abs(line[i] - line[i + 1]) > 1) return false;
-
-            if (line[i] > line[i + 1]) { // 내리막 경사로 설치
-                for (int j = i + 1; j <= i + X; j++) {
-                    if (j >= N || line[j] != line[i + 1] || installed[j]) return false;
-                    installed[j] = true;
-
-                }
-            } else { // 오르막 경사로 설치
-                for (int j = i; j > i - X; j--) {
-                    if (j < 0 || line[j] != line[i] || installed[j]) return false;
-                    installed[j] = true;
-                }
-            }
-        }
-        return true;
+    
+    //경사로 췌크~
+    public static boolean check(int[] line , int X) {
+		int N = line.length;
+		boolean[] installed = new boolean[N];
+		
+		for(int i = 0 ; i < N - 1; i++) {
+			if(line[i] == line[i+1]) continue; //높이가 같을 때
+			
+			if(Math.abs(line[i] - line[i+1]) > 1) return false; //높이차이가 1보다 큼
+			
+			if(line[i] > line[i+1]) {//내리막
+				for(int j =i+1; j<= i+X; j++) {
+					if(j >= N || line[j] != line[i+1] || installed[j]) return false;
+					installed[j] = true;
+				}
+			} else {
+				for(int j = i; j > i-X; j--) { //오르막
+					if(j < 0 || line[j] != line[i] || installed[j]) return false;
+					installed[j] = true;
+				}
+			}
+		}
+		return true;
     }
+    
+    
 }
