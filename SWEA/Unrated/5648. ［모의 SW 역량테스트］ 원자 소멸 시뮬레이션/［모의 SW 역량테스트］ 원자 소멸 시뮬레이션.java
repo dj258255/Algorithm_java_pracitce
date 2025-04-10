b090e8de61b64a35ebef89b6bd57e9278000005f
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-10 13:24:06 +0900

    [Unrated] Title: [모의 SW 역량테스트] 원자 소멸 시뮬레이션, Time: 2,695 ms, Memory: 291,288 KB -BaekjoonHub

--- SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
+++ SWEA/Unrated/5648. ［모의 SW 역량테스트］ 원자 소멸 시뮬레이션/［모의 SW 역량테스트］ 원자 소멸 시뮬레이션.java
@@ -3,9 +3,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Solution {
-    static final int SIZE = 4001;
-    static final int OFFSET = 1000;
-    static final int SCALE = 2;
+    static int SIZE = 4001; //좌표 확장을 위해 2배 + 1000
 
     static int[] dx = {0, 0, -1, 1}; //상하좌우
     static int[] dy = {1, -1, 0, 0};
@@ -31,8 +29,8 @@ public class Solution {
 
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                int x = (Integer.parseInt(st.nextToken()) + OFFSET) * SCALE;
-                int y = (Integer.parseInt(st.nextToken()) + OFFSET) * SCALE;
+                int x = (Integer.parseInt(st.nextToken()) + 1000) * 2;
+                int y = (Integer.parseInt(st.nextToken()) + 1000) * 2;
                 int dir = Integer.parseInt(st.nextToken());
                 int k = Integer.parseInt(st.nextToken());
                 cores.add(new Core(x, y, dir, k));
@@ -45,7 +43,7 @@ public class Solution {
 
     static int simulate(List<Core> cores) {
         int totalEnergy = 0;
-        int[][] map = new int[SIZE][SIZE];
+        int[][] map = new int[SIZE][SIZE]; //위치에 존재하는 에너지 총합
 
         while (!cores.isEmpty()) {
             //원자 이동
@@ -54,23 +52,28 @@ public class Solution {
                 c.y += dy[c.dir];
             }
 
+            //map 초기화
             for (Core c : cores) {
                 if (c.x >= 0 && c.x < SIZE && c.y >= 0 && c.y < SIZE) {
                     map[c.x][c.y] += c.k;
                 }
             }
 
+            //충돌 확인 및 에너지 합산
             List<Core> next = new ArrayList<>();
             for (Core c : cores) {
                 if (c.x < 0 || c.x >= SIZE || c.y < 0 || c.y >= SIZE) continue;
 
                 if (map[c.x][c.y] == c.k) {
+                    //충돌 안함
                     next.add(c);
                 } else {
+                    //충돌:이 에너지는 여기서 더해지고, 제거됨
                     totalEnergy += c.k;
                 }
             }
 
+            //map초기화 (다음 시간 대비)
             for (Core c : cores) {
                 if (c.x >= 0 && c.x < SIZE && c.y >= 0 && c.y < SIZE) {
                     map[c.x][c.y] = 0;
