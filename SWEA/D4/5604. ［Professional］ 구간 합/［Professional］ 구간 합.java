176c20a6760fe20bdecb9d307ced3236311da099
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-15 20:09:47 +0900

    [D4] Title: [Professional] 구간 합, Time: 82 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D4/5604. ［Professional］ 구간 합/［Professional］ 구간 합.java
+++ SWEA/D4/5604. ［Professional］ 구간 합/［Professional］ 구간 합.java
@@ -1,47 +1,55 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            long A = Long.parseLong(st.nextToken());
-            long B = Long.parseLong(st.nextToken());
-            
-            long answer = digitSum(B) - (A == 0 ? 0 : digitSum(A - 1));
-            
-            System.out.println("#" + tc + " " + answer);
-        }
-    }
-    
+	static long ans;
 
-    static long digitSum(long number) {
-        if (number < 10) { // 10미만이면 그만.
-            return number * (number + 1) / 2;
-        }
-        
-        //자릿수 구하기
-        int n = Long.toString(number).length();
-        
-        long p = (long) Math.pow(10, n - 1);
-        long quotient = number / p; //최고로 높은 자릿수 숫자
-        long remainder = number % p; //나머지(보통은 0)
-        
-        //0부터 p-1까지의 자리 합
-        long sum = 45L*(n-1)*(p/10);
-//      0 ~ 9 / 45 = 45 * 1
-//      0 ~ 99 / 900 = 45 * 20
-//      0 ~ 999 / 13500 = 45 * 300
-//      0 ~ 9999 / 180000 = 45 * 4000
-     
-        return quotient * sum
-               + (quotient * (quotient - 1) / 2) * p 
-               + quotient * (remainder + 1) 
-               + digitSum(remainder);
-    }
+	static void calc(long n, long t){
+		while(n>0){
+			ans+=(n%10)*t;
+			n/=10;
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int T = Integer.parseInt(st.nextToken());
+		for(int tc=1; tc<=T; tc++){
+			st = new StringTokenizer(br.readLine());
+			long A= Long.parseLong(st.nextToken());
+			long B= Long.parseLong(st.nextToken());
+			ans=0;
+			long mul=1;
+			while(A<=B){
+				while(B%10!=9 && A<=B){
+					calc(B,mul);//(21,1)=0+ 1+2=3 //(1,10)=67+ 10=77
+					B--;        //(20,1)=3+ 0+2=5 
+				}//19 //0
+				if(A>B) break; //8>19 //1>0
+				while(A%10!=0 && A<=B){
+					calc(A,mul);//(8,1)=5+  8=13
+					A++;        //(9,1)=13+ 9=22
+				}//10
+				A/=10;//10->1
+				B/=10;//19->1
+				long m=(B-A+1)*mul;//(1-1+1)*1=1
+				for(int i=0; i<10; i++) ans+=m*i;//22+ 1*0+1*1+1*2+1*3+1*4+1*5+1*6+1*7+1*8+1*9=22+ 45=67
+				mul*=10;//1*10=10
+			}
+			System.out.println("#"+tc+" "+ans);//77
+		}
+	}	
 }
+/*
+                             A
+                             8  9 
+    10 11 12 13 14 15 16 17 18 19 
+    A                           B
+    20 21
+        B
+                             8+9+    
+     +10(1이 10개)
+     +45(1~9)    
+    +2+0+2+1=77
+*/
