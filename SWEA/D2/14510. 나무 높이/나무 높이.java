b6bfe38189a3b8d4a42366b381b46fd75026925c
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-17 16:39:18 +0900

    [D2] Title: 나무 높이, Time: 93 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -3,56 +3,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
-
 public class Solution{
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int tc = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
 		
-		for(int t= 1; t<=tc; t++) {
+		for(int tc = 1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			
-			int[] number = new int[N+1];
+			int[] number = new int[N];
+			int highTree =0;
 			
 			st = new StringTokenizer(br.readLine());
-			
-			int highTree =0;
 			for(int i = 0 ; i < N; i++) {
 				number[i] = Integer.parseInt(st.nextToken()); 
 				highTree = Math.max(highTree, number[i]);
 			}
 			
-			int oddDay=0;
-			int evenday=0;
-			
+			int oddDay =0;
+			int evenDay =0;
 			for(int i = 0 ; i < N; i++) {
-				int temp = highTree-number[i];
-				oddDay += temp%2;
-				evenday += temp/2;
+				oddDay += (highTree-number[i])%2;
+				evenDay += (highTree-number[i])/2;
 			}
 			
-			int left = 0 ;
-			int right = N*highTree;
+			
+			
+			int left=0;
+			int right=highTree*N;
+			
 			while(left <= right) {
 				int mid = (left+right)/2;
+				
 				int midOdd = (mid+1)/2;
-				int midEven = mid/2;
+				int midEven = (mid)/2;
 				
-				int oddNeed = 2 * Math.max(0, evenday -midEven );
+				int needOdd = 2 * Math.max(0, evenDay-midEven );
 				
-				if(oddNeed+oddDay <= midOdd) {
-					right = mid-1;
+				if(needOdd+oddDay > midOdd) {
+					left = mid + 1;
 				} else {
-					left = mid+1;
+					right = mid-1;
+					
 				}
+				
 			}
 			
-			System.out.println("#" + t + " " + left);
-			
+			System.out.println("#" + tc + " " +left);
 		}
 	}
 }
