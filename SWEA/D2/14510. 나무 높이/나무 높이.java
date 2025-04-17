cb2b01758ed4de7fed258b99a361a16b0a8ed3cf
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-17 09:08:38 +0900

    [D2] Title: 나무 높이, Time: 87 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -3,54 +3,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        
-        for (int tc = 1; tc <= T; tc++) {
-        	StringTokenizer st = new StringTokenizer(br.readLine());
-        	int N = Integer.parseInt(st.nextToken());
-        	
-        	st = new StringTokenizer(br.readLine());
-        	int[] num = new int[N];
-        	
-        	int highLegnth =0;
-        	for(int i = 0 ; i < N; i++) {
-        		num[i] = Integer.parseInt(st.nextToken());
-        		highLegnth = Math.max(num[i], highLegnth);
-        	}
-        	
-        	
-        	int oddDay=0;
-        	int evenDay =0;
-        	for(int i = 0 ; i < N; i++) {
-        		int temp = highLegnth - num[i];
-        		evenDay += temp/2; //필요 짝수 날
-        		oddDay += temp%2; // 필요 홀 수 날
-        	}
-        	
-        	
-        	int left = 0;
-        	int right = 12000000;
-        	while (left <= right) {
-        	    int mid = (left + right) / 2;
-        	    
-        	    int leftAvail = (mid+1)/2;
-        	    int rightAvail = (mid)/2;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
 
-        	    long extraOdd = 2 * Math.max(0, evenDay - rightAvail);
-
-        	    if(oddDay + extraOdd <= leftAvail) {
-        	    	right = mid-1;
-        	    } else {
-        	    	left = mid+1;
-        	    }
-        	}
-
-        	System.out.println("#" + tc + " " + left);
-
-        	
-        }
-    }
+public class Solution{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int tc = Integer.parseInt(st.nextToken());
+		
+		for(int t= 1; t<=tc; t++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			
+			int[] number = new int[N+1];
+			
+			st = new StringTokenizer(br.readLine());
+			
+			int highTree =0;
+			for(int i = 0 ; i < N; i++) {
+				number[i] = Integer.parseInt(st.nextToken()); 
+				highTree = Math.max(highTree, number[i]);
+			}
+			
+			int oddDay=0;
+			int evenday=0;
+			
+			for(int i = 0 ; i < N; i++) {
+				int temp = highTree-number[i];
+				oddDay += temp%2;
+				evenday += temp/2;
+			}
+			
+			int left = 0 ;
+			int right = N*highTree;
+			while(left <= right) {
+				int mid = (left+right)/2;
+				int midOdd = (mid+1)/2;
+				int midEven = mid/2;
+				
+				int oddNeed = 2 * Math.max(0, evenday -midEven );
+				
+				if(oddNeed+oddDay <= midOdd) {
+					right = mid-1;
+				} else {
+					left = mid+1;
+				}
+			}
+			
+			System.out.println("#" + t + " " + left);
+			
+		}
+	}
 }
