a4274c435ba3edaa7292522ad53b5bf96c6a46c1
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-18 15:48:02 +0900

    [D4] Title: 오름차순 줄 세우기, Time: 220 ms, Memory: 94,572 KB -BaekjoonHub

--- SWEA/D4/3263. 오름차순 줄 세우기/오름차순 줄 세우기.java
+++ SWEA/D4/3263. 오름차순 줄 세우기/오름차순 줄 세우기.java
@@ -3,47 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(br.readLine().trim());
-        
-        for (int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine().trim());
-            int[] arr = new int[N];
-            
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-            
-            int[] pos = new int[N + 1];
-            for (int i = 0; i < N; i++) {
-                pos[arr[i]] = i; //pos[번호] = 인덱스
-            }
-            
-            
-            int maxLength = 1;
-            int curLength = 1;
-            
-            // 번호 2부터 N까지 순회하며, 이전 번호보다 뒤에 있으면 연속
-            for (int i = 2; i <= N; i++) {
-                if (pos[i - 1] < pos[i]) { // 앞의 번호가 현재 번호보다 앞쪽에 있다면
-                    curLength++;
-                } else {
-                    curLength = 1;  // 연속이 끊기면 새롭게 시작
-                }
-                
 
-                if (curLength > maxLength) {
-                    maxLength = curLength;
-                }
-                
-            }
-            
-            int answer = N - maxLength;
-            System.out.println("#" + tc + " " + answer);
-        }
-    }
+public class Solution{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int tc = Integer.parseInt(st.nextToken());
+		
+		for(int t = 1; t<=tc; t++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			int[] number = new int[N];
+			int[] dp = new int[N+1];
+			for(int i = 0 ; i < N; i++) {
+				number[i] = Integer.parseInt(st.nextToken()); 
+				dp[number[i]] = i;
+			}
+			
+			int maxLength = 1;
+			int curLength = 1;
+			for(int i = 2 ; i <= N; i++) {
+				if(dp[i-1] < dp[i] ) {
+					curLength++;
+				} else {
+					 curLength = 1;
+				}
+				
+				maxLength = Math.max(maxLength, curLength);
+               
+			}
+			
+			System.out.println("#" + t + " " + (N-maxLength));
+		}
+	}
 }
+
