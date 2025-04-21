37cf1204e240301e18b2df69e82115a9913cd0ed
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-21 12:16:57 +0900

    [D4] Title: 오름차순 줄 세우기, Time: 204 ms, Memory: 95,948 KB -BaekjoonHub

--- SWEA/D4/3263. 오름차순 줄 세우기/오름차순 줄 세우기.java
+++ SWEA/D4/3263. 오름차순 줄 세우기/오름차순 줄 세우기.java
@@ -3,41 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Solution{
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int tc = Integer.parseInt(st.nextToken());
+		int Tc = Integer.parseInt(st.nextToken());
 		
-		for(int t = 1; t<=tc; t++) {
+		for(int t = 1 ; t <= Tc; t++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			
-			st = new StringTokenizer(br.readLine());
-			int[] number = new int[N];
+			int[] kids = new int[N];
 			int[] dp = new int[N+1];
+			st = new StringTokenizer(br.readLine());
 			for(int i = 0 ; i < N; i++) {
-				number[i] = Integer.parseInt(st.nextToken()); 
-				dp[number[i]] = i;
+				kids[i] = Integer.parseInt(st.nextToken());
+				dp[kids[i]] = i; //dp[i] 는 dp[kids[i]]의 값
 			}
 			
-			int maxLength = 1;
-			int curLength = 1;
-			for(int i = 2 ; i <= N; i++) {
+			
+			int maxLength =1;
+			int length = 1;
+			for(int i = 2; i <= N; i++) {
 				if(dp[i-1] < dp[i] ) {
-					curLength++;
+					length++;
 				} else {
-					 curLength = 1;
+					length = 1;
 				}
 				
-				maxLength = Math.max(maxLength, curLength);
-               
+				
+				maxLength = Math.max(length, maxLength);
 			}
 			
 			System.out.println("#" + t + " " + (N-maxLength));
 		}
 	}
 }
-
