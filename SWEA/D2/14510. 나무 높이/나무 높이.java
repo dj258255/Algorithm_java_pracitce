a99c8cd6ed0d75a311b511daf26c8fd9063adf28
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-22 14:26:21 +0900

    [D2] Title: 나무 높이, Time: 99 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -4,55 +4,71 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution{
+	static BufferedReader br;
+	static StringTokenizer st;
+	
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int T = Integer.parseInt(st.nextToken());
-		
-		for(int tc = 1; tc<=T; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			
-			int[] number = new int[N];
-			int highTree =0;
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0 ; i < N; i++) {
-				number[i] = Integer.parseInt(st.nextToken()); 
-				highTree = Math.max(highTree, number[i]);
-			}
-			
-			int oddDay =0;
-			int evenDay =0;
-			for(int i = 0 ; i < N; i++) {
-				oddDay += (highTree-number[i])%2;
-				evenDay += (highTree-number[i])/2;
-			}
-			
-			
-			
-			int left=0;
-			int right=highTree*N;
-			
-			while(left <= right) {
-				int mid = (left+right)/2;
-				
-				int midOdd = (mid+1)/2;
-				int midEven = (mid)/2;
-				
-				int needOdd = 2 * Math.max(0, evenDay-midEven );
-				
-				if(needOdd+oddDay > midOdd) {
-					left = mid + 1;
-				} else {
-					right = mid-1;
-					
-				}
-				
-			}
-			
-			System.out.println("#" + tc + " " +left);
+		int Tc = Integer.parseInt(br.readLine().trim());
+		for(int t = 1; t<= Tc; t++) {
+			//입력
+			init();
+			//풀이
+			solve();
+			//출력
+			System.out.println("#" + t + " " + left);
 		}
+	}
+	
+	static int left,right;
+	static int oddDay,evenDay;
+	public static void solve() {
+		oddDay = 0;
+		evenDay =0;
+		for(int i = 0 ; i < N; i++) {
+			int diffTree = highTree-trees[i];
+			oddDay += diffTree%2;
+			evenDay += diffTree/2;
+		}
+		
+		
+		
+		
+		left = 0;
+		right = highTree*N;
+		
+		while(left <= right) {
+			int mid = (left+right) / 2;
+			int midOdd = (mid+1)/2;
+			int midEven = (mid)/2;
+			
+			int oddNeed = 2 * Math.max(0, evenDay - midEven);
+			
+			if(oddNeed+oddDay > midOdd) {
+				left = mid+1;
+			} else {
+				right = mid-1;
+			}
+			
+		}
+		
+	}
+	
+	static int N,highTree;
+	static int[] trees;
+	public static void init() throws IOException{
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		
+		trees = new int[N];
+		
+		highTree = Integer.MIN_VALUE;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N; i++) {
+			trees[i] = Integer.parseInt(st.nextToken());
+			highTree = Math.max(highTree, trees[i]);
+		}
+		
 	}
 }
