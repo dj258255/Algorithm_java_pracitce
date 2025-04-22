89084c3c54d497df8167b246b244cb0311345f14
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-22 14:27:17 +0900

    [D2] Title: 나무 높이, Time: 93 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -3,72 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution{
-	static BufferedReader br;
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException{
-		br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int Tc = Integer.parseInt(br.readLine().trim());
-		for(int t = 1; t<= Tc; t++) {
-			//입력
-			init();
-			//풀이
-			solve();
-			//출력
-			System.out.println("#" + t + " " + left);
-		}
-	}
-	
-	static int left,right;
-	static int oddDay,evenDay;
-	public static void solve() {
-		oddDay = 0;
-		evenDay =0;
-		for(int i = 0 ; i < N; i++) {
-			int diffTree = highTree-trees[i];
-			oddDay += diffTree%2;
-			evenDay += diffTree/2;
-		}
-		
-		
-		
-		
-		left = 0;
-		right = highTree*N;
-		
-		while(left <= right) {
-			int mid = (left+right) / 2;
-			int midOdd = (mid+1)/2;
-			int midEven = (mid)/2;
-			
-			int oddNeed = 2 * Math.max(0, evenDay - midEven);
-			
-			if(oddNeed+oddDay > midOdd) {
-				left = mid+1;
-			} else {
-				right = mid-1;
-			}
-			
-		}
-		
-	}
-	
-	static int N,highTree;
-	static int[] trees;
-	public static void init() throws IOException{
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		
-		trees = new int[N];
-		
-		highTree = Integer.MIN_VALUE;
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0 ; i < N; i++) {
-			trees[i] = Integer.parseInt(st.nextToken());
-			highTree = Math.max(highTree, trees[i]);
-		}
-		
-	}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int Tc = Integer.parseInt(br.readLine().trim());
+        for (int t = 1; t <= Tc; t++) {
+            // 입력
+            int N = Integer.parseInt(br.readLine().trim());
+            int[] trees = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int highTree = Integer.MIN_VALUE;
+            for (int i = 0; i < N; i++) {
+                trees[i] = Integer.parseInt(st.nextToken());
+                if (trees[i] > highTree) {
+                    highTree = trees[i];
+                }
+            }
+            
+            // 그리디 계산
+            int oddDay = 0, evenDay = 0;
+            for (int i = 0; i < N; i++) {
+                int diff = highTree - trees[i];
+                oddDay  += diff % 2;
+                evenDay += diff / 2;
+            }
+            // 짝수일 물주기 과잉분을 홀수일 물주기로 전환
+            while (evenDay - oddDay > 1) {
+                evenDay--;
+                oddDay += 2;
+            }
+            int result = (oddDay > evenDay)
+                       ? 2 * oddDay - 1
+                       : 2 * evenDay;
+            
+            // 출력
+            System.out.println("#" + t + " " + result);
+        }
+        br.close();
+    }
 }
