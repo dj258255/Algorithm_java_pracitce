993ebb76b584073b7c04f325df2776258b3fe0c7
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-23 22:45:01 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 중위순회, Time: 166 ms, Memory: 22,640 KB -BaekjoonHub

--- SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
+++ SWEA/D4/1231. ［S／W 문제해결 기본］ 9일차 － 중위순회/［S／W 문제해결 기본］ 9일차 － 중위순회.java
@@ -1,52 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Solution {
+public class Solution{
+	static class Node{
+		char data;
+		Node left;
+		Node right;
+		
+		public Node(char c) {
+			this.data = c;
+		}
+	}
+	
+	
+	public static void search(Node node) {
+		if(node == null) return; //리턴
+		search(node.left);
+		sb.append(node.data);
+		search(node.right);
+	}
+	
+	
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static int N;
+	static Node[] nodes;
+	public static void input() throws IOException{
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		
+		
+		nodes = new Node[N+1];
+		
+		for(int i = 1 ; i<= N; i++) {
+			nodes[i] = new Node(' ');
+		}
+		
+		for(int i = 0; i < N; i++) {
 
-    static class Node {
-        char data;
-        Node left, right;
-        Node(char data) { this.data = data; }
-    }
+			
+			st = new StringTokenizer(br.readLine());
+			int index = Integer.parseInt(st.nextToken());
+			nodes[index].data = st.nextToken().charAt(0);
+			
+			if(st.hasMoreTokens()) {
+				int a = Integer.parseInt(st.nextToken());
+				nodes[index].left = nodes[a];
+			}
+			
+			if(st.hasMoreTokens()) {
+				int a = Integer.parseInt(st.nextToken());
+				nodes[index].right = nodes[a];
+			}
+		}
+		
 
-    static Node[] nodes;
-    static StringBuilder sb;
-
-    static void inorder(Node node) {
-        if (node == null) return;
-        inorder(node.left);
-        sb.append(node.data);
-        inorder(node.right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        for (int tc = 1; tc <= 10; tc++) {
-            int N = Integer.parseInt(br.readLine());
-            nodes = new Node[N+1];
-            for (int i = 1; i <= N; i++) {
-                nodes[i] = new Node(' ');  
-            }
-
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
-                int idx = Integer.parseInt(st.nextToken());
-                nodes[idx].data = st.nextToken().charAt(0);
-                if (st.hasMoreTokens()) {
-                    int l = Integer.parseInt(st.nextToken());
-                    nodes[idx].left = nodes[l];
-                    if (st.hasMoreTokens()) {
-                        int r = Integer.parseInt(st.nextToken());
-                        nodes[idx].right = nodes[r];
-                    }
-                }
-            }
-
-            sb = new StringBuilder();
-            inorder(nodes[1]);
-            System.out.println("#" + tc + " " + sb.toString());
-        }
-    }
+				
+		
+	}
+	
+	public static void main(String[] args) throws IOException{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		for(int t = 1; t<= 10; t++) {
+			
+			//입
+			input();
+			
+			//실행
+			sb = new StringBuilder();
+			search(nodes[1]);
+			
+			//출
+			System.out.println("#" + t + " " + sb.toString());
+		}
+	}
 }
