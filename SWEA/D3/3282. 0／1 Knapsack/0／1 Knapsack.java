34e393863e89364462d97a72d1a0192591e46d01
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-04-27 16:46:46 +0900

    [D3] Title: 0/1 Knapsack, Time: 96 ms, Memory: 27,624 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -3,34 +3,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+public class Solution {
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	static int N;
+	static int K;
+	static int[] V,C;
+	static int[]dp;
+	public static void input() throws IOException{
+		st = new StringTokenizer(br.readLine().trim());
+		N = Integer.parseInt(st.nextToken());//물건
 		
-		int Tc = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());//부피
 		
-		for(int t= 1; t <= Tc; t++) {
+		V = new int[N];
+		C = new int[N];
+		for(int i = 0 ; i < N ; i ++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			int[] V = new int[N];
-			int[] C = new int[N];
-			for(int i = 0 ; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				V[i] = Integer.parseInt(st.nextToken());
-				C[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			int[] dp = new int[K+1];
-			
-			for(int i = 0 ; i < N; i++) {
-				for(int j = K; j >= V[i]; j--) {
-					dp[j] = Math.max(dp[j], dp[j - V[i]] + C[i] );
-				}
-			}
-			
-			System.out.println("#" + t + " " + dp[K]);
+			V[i] = Integer.parseInt(st.nextToken());//부피
+			C[i] = Integer.parseInt(st.nextToken());//가치
 		}
+	}
+	
+	public static void dp_solve() {
+		dp = new int[K+1];
+		
+		for(int i = 0 ; i < N; i++) {
+			for(int j = K; j >= V[i]; j--) {
+				dp[j] = Math.max(dp[j], dp[j- V[i]] + C[i]);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		int Tc = Integer.parseInt(br.readLine().trim());
+		for(int t = 1 ; t<=Tc; t++) {
+			//입력
+			input();
+			//계산
+			dp_solve();
+			//결과
+			sb.append("#").append(t).append(" ").append(dp[K]).append("\n");
+		}
+		System.out.println(sb.toString());
 	}
 }
