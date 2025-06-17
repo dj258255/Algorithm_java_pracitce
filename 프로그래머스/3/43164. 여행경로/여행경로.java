eb1eaf1155a598a99b35fc6cc33d1ed7524bcf47
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-06-18 02:45:33 +0900

    [level 3] Title: 여행경로, Time: 0.39 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,15 +1,28 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 class Solution {
     public static List<String> route = new ArrayList<>();
-
+    
     public String[] solution(String[][] tickets) {
         Map<String, PriorityQueue<String>> map = new HashMap<>();
 
-        for (String[] ticket : tickets) {
-            map.computeIfAbsent(ticket[0], k -> new PriorityQueue<>()).add(ticket[1]);
+        for(int i = 0 ; i < tickets.length; i++){
+            String start = tickets[i][0];
+            String goal = tickets[i][1];
+            
+            if(!map.containsKey(start)){
+                map.put(start, new PriorityQueue<>());
+            }
+            map.get(start).add(goal);
         }
-
+        
         dfs("ICN", map);
 
         Collections.reverse(route); // 또는 route.add(0, airport) 방식으로도 가능
