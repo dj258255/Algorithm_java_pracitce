05c59336895daeee7b2ac0a8347e80fb7a1b3239
Beom_ <30296121+dj258255@users.noreply.github.com>
2025-06-20 18:56:08 +0900

    [level 2] Title: 오픈채팅방, Time: 166.07 ms, Memory: 172 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,35 +1,48 @@
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
     public String[] solution(String[] record) {
+        
+        //record -> 첫 단어 "Enter" , "Leave" , "Change"
+        //두번째 [유저아이디]
+        //세번째 [낙네임] 
+        
+        int count = 0;
         Map<String, String> map = new HashMap<>();
-        ArrayList<String> messages = new ArrayList<>();
-
-        for (String log : record) {
-            String[] parts = log.split(" ");
-            String status = parts[0];
-            String uid = parts[1];
-
-            if (status.equals("Enter") || status.equals("Change")) {
-                String nickname = parts[2];
-                map.put(uid, nickname);
+        
+        for (int i = 0 ; i < record.length; i++){
+            String[] temp = record[i].split(" ");
+            
+            if (temp[0].equals("Leave")){
+                continue;
+            } else if (temp[0].equals("Enter")){
+                map.put(temp[1], temp[2]);
+            } else { //Change
+                map.put(temp[1], temp[2]);
+                count++;
             }
         }
-
-        for (String log : record) {
-            String[] parts = log.split(" ");
-            String status = parts[0];
-            String uid = parts[1];
-
-            if (status.equals("Enter")) {
-                messages.add(map.get(uid) + "님이 들어왔습니다.");
-            } else if (status.equals("Leave")) {
-                messages.add(map.get(uid) + "님이 나갔습니다.");
+        
+        String[] answer = new String[record.length - count];
+        
+        int idx = 0;
+        
+        for(int i = 0 ; i < record.length; i++){
+            String[] temp = record[i].split(" ");
+            String nickname = map.get(temp[1]);
+            
+            if(temp[0].equals("Enter")){
+                answer[idx] = nickname + "님이 들어왔습니다.";
+                idx++;
+            } else if(temp[0].equals("Leave")){
+                answer[idx] = nickname + "님이 나갔습니다.";
+                idx++;
             }
-        }
 
-        return messages.toArray(new String[0]);
+        }
+        
+        return answer;
     }
 }
+//리스트 닉네임 아이디 매핑
