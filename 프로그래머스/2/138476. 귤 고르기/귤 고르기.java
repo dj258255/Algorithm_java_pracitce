8fc6e474a982b6c4db65366f890fec2a894f0c65
BeomSu <30296121+dj258255@users.noreply.github.com>
2026-02-02 17:34:00 +0900

    [level 2] Title: 귤 고르기, Time: 70.04 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,24 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
+        // 1. 크기별 개수 카운팅
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int t : tangerine) {
+            count.put(t, count.getOrDefault(t, 0) + 1);
+        }
+        
+        // 2. 개수만 뽑아서 내림차순 정렬
+        List<Integer> counts = new ArrayList<>(count.values());
+        counts.sort(Collections.reverseOrder());
+        
+        // 3. 많은 것부터 k개 채우기
         int answer = 0;
-        Arrays.sort(tangerine);
-        int[] arr = new int[tangerine[tangerine.length - 1]];
-        
-        for(int i = 0 ; i < tangerine.length ; i++){
-            arr[tangerine[i] - 1]++;
-        }
-        
-        Arrays.sort(arr);
-        int index = arr.length - 1;
-        while(true){
-            k = k - arr[index];
+        for (int c : counts) {
+            k -= c;
             answer++;
-            index--;
-            if(k <= 0){
-                break;
-            }
+            if (k <= 0) break;
         }
+        
         return answer;
     }
 }
