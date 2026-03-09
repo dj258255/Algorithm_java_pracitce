402c877f2eb1753d1a3d5a453ed7c0bf9294f622
BeomSu <30296121+dj258255@users.noreply.github.com>
2026-03-09 10:40:50 +0900

    [백준 #1679] 숫자놀이 (Java)

--- 백준/Silver I/1679. 숫자놀이/Main.java
+++ 백준/Silver I/1679. 숫자놀이/Main.java
@@ -5,80 +5,78 @@ public class Main {
     static BufferedReader br;
     static BufferedWriter bw;
     static StringTokenizer st;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         input();
         solve();
         output();
     }
 
-    static int N, K;
+    static int N,K;
     static int[] num;
-
-    public static void input() throws IOException {
+    public static void input() throws IOException{
         br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
         st = new StringTokenizer(br.readLine().trim());
         N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine().trim());
         num = new int[N];
-        for (int i = 0; i < N; i++) num[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0 ; i < N ; i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
         st = new StringTokenizer(br.readLine().trim());
         K = Integer.parseInt(st.nextToken());
-        endGame = false;
     }
 
+    static String answerName;
     static boolean endGame;
-
-    public static void solve() throws IOException {
-        boolean isHolsoon = true;
+    public static void solve() throws IOException{
+        boolean isHol = true;
         int goal = 1;
-        while (true) {
-            bfs(isHolsoon ? "holsoon" : "jjaksoon", goal);
-            if (endGame) {
-                bw.write(winName + " win at " + goal);
+        while(true){
+            bfs(isHol ? "holsoon" : "jjaksoon", goal);
+            if(endGame){
+                bw.write(answerName + " win at " + goal);
                 return;
             }
             goal++;
-            isHolsoon = !isHolsoon;
+            isHol = !isHol;
         }
     }
 
-    static String winName;
-
-    public static void bfs(String name, int goal) {
+    public static void bfs(String name, int goal){
         Queue<int[]> queue = new ArrayDeque<>();
         Set<Integer> set = new HashSet<>();
-        queue.add(new int[]{0, 0});
         set.add(0);
+        queue.add(new int[]{0, 0});
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int[] node = queue.poll();
-            int curN  = node[0];
-            int count = node[1];
+            int curNum = node[0];
+            int count  = node[1];
 
-            for (int n : num) {
-                int nextNum = curN + n;
+            if(count >= K) continue;
 
-                if (nextNum == goal) {
-                    winName = name;
+            for(int i = 0; i < num.length; i++){
+                int nextNum = curNum + num[i];
+
+                if(nextNum == goal){
+                    answerName = name;
                     return;
                 }
 
-                if (set.contains(nextNum)) continue;
-                if (nextNum > goal) continue;
-                if (count + 1 >= K) continue;  // K번 초과 방지
+                if(nextNum > goal) continue;
+                if(set.contains(nextNum)) continue;
 
-                set.add(nextNum);  // curN 아니라 nextNum
+                set.add(nextNum);
                 queue.add(new int[]{nextNum, count + 1});
             }
         }
 
-        winName = name.equals("holsoon") ? "jjaksoon" : "holsoon";
+        answerName = name.equals("holsoon") ? "jjaksoon" : "holsoon";
         endGame = true;
     }
 
-    public static void output() throws IOException {
+    public static void output() throws IOException{
         bw.flush();
         bw.close();
     }
