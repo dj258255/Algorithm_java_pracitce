a1e21cbf8753981ba537f6b38e1835cfd4208ca5
BeomSu <30296121+dj258255@users.noreply.github.com>
2026-03-19 22:15:05 +0900

    [level 1] Title: 노란불 신호등, Time: 72.58 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -1,23 +1,25 @@
 class Solution {
     public int solution(int[][] signals) {
-        for(int i = 0; i < 10000000; i++){
-            boolean allYellow = true;
-            
-            for(int j = 0; j < signals.length; j++) {
+        
+        for(int i = 0 ; i < 10000000; i++){
+            boolean possible = true;
+            for(int j = 0 ; j < signals.length; j++){
                 int g = signals[j][0];
                 int y = signals[j][1];
                 int r = signals[j][2];
-                int period = g + y + r;
+                int period = g+y+r;
                 
-                int position = (i - 1) % period;
+                int curY = (i-1) % period;
                 
-                if(!(position >= g && position < g + y)) {
-                    allYellow = false;
+                if(!(curY >= g && curY < g + y)) {
+                    possible = false;
                     break;
                 }
+                
             }
             
-            if(allYellow) return i;
+            
+            if(possible) return i;
         }
         
         return -1;
