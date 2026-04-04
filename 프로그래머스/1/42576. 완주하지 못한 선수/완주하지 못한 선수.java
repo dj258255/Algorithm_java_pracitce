03724d6c579540a24e5e65d3f513b8ff7274922e
BeomSu <30296121+dj258255@users.noreply.github.com>
2026-04-04 20:39:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 116.60 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,25 @@
-import java.util.HashMap;
+import java.util.*;
+import java.io.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        String answer = "";
+        Map<String,Integer> participants = new HashMap<>();
+        for(int i = 0 ; i < participant.length; i++){
+         //   participants.computeIfAbsnet(participant[i] , participants.getOrDefault(0,));
+            participants.put(participant[i], participants.getOrDefault(participant[i],0)+1);
         }
         
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
+        for(int i = 0 ; i < completion.length; i++){
+            participants.put(completion[i] , participants.get(completion[i])-1);
         }
         
-        for (String name : map.keySet()) {
-            if (map.get(name) > 0) {
-                return name; 
+        for(String name : participants.keySet()){
+            if(participants.get(name) > 0){
+                answer = name;
             }
         }
         
-        return "";
+        return answer;
     }
 }
