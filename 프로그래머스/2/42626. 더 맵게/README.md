d5fc2a3d9a4f977d945e5dca5c1660685456ca5a
BeomSu <30296121+dj258255@users.noreply.github.com>
2026-04-05 18:32:53 +0900

    [level 2] Title: 더 맵게, Time: 1652.94 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 129 MB, 시간: 1720.62 ms
+메모리: 129 MB, 시간: 1652.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 03일 23:21:29
+2026년 04월 05일 18:32:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,28 +1,26 @@
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
+        int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int i = 0 ; i < scoville.length; i++){
-            pq.add(scoville[i]);
+        for(int sco : scoville){
+            pq.add(sco);
         }
         
-        int answer =0;
-        
-        while(!pq.isEmpty() && pq.peek()<K){
-            if (pq.size() < 2) {
-                return -1;
-            }
-            
-            int first = pq.poll();
-            int second = pq.poll();
-            
-            int temp = first+(2*second);
-            pq.offer(temp);
-            
+        while(!pq.isEmpty()){
+            if(pq.peek() >= K) return answer;
+            if(pq.size() < 2) return -1;
+            int sco1 = pq.poll();
+            int sco2 = pq.poll();
             answer++;
+            
+            pq.add(sco1+(sco2*2));
+            
         }
+        //섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+        
         
         return answer;
     }
