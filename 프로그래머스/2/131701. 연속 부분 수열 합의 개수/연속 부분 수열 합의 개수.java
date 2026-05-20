6681c19b6ebc063ab10c2ddb28d48dcd16b7e0e4
BeomSu <30296121+dj258255@users.noreply.github.com>
2026-05-20 15:01:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 102.65 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,17 +3,18 @@ import java.io.*;
 
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> set = new HashSet<>();
         
+        Set<Integer> set = new HashSet<>();
         int n = elements.length;
-        for(int i = 0; i < n; i++){ //한번씩 돌아가기
+        for(int i = 0 ; i < n; i++){ //현재부터 시작 부분 수열 더하자
             int sum = 0;
-            for(int j = 0 ; j < n; j++){ //크기
+            for(int j = 0 ; j < n; j++){ //n까지의 부분수열을 더해보자 
                 int element = elements[(i+j)%n];
                 sum += element;
                 set.add(sum);
             }
         }
+        
         
         return set.size();
     }
